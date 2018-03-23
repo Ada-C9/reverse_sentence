3d09a4058abfa358db1b5c56fdf5fe92fe44331d
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-22 22:55:44 -0700

    Used a combination of existing string-reverse and word-reverse methods and got this to pass on the first try. W00t.

--- specs/reverse_sentence_spec.rb
+++ specs/reverse_sentence_spec.rb
@@ -1,6 +1,11 @@
+require 'minitest/skip_dsl'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'pry'
+
 require_relative '../lib/reverse_sentence'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "reverse sentence" do
   describe "basic tests" do
