b1a65ffa80140e6866d284611bf98b4f7850b337
Tor Shimizu <torshimizu@gmail.com>
2018-03-26 11:52:50 -0700

    working reverse_sentence method - reverse whole sentence, then reverse each word

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,54 @@
 # A method to reverse the words in a sentence, in place.
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return if my_sentence == nil
+  length = my_sentence.length
+  return if length < 2
+
+  front_counter = 0
+  back_counter = length - 1
+  while front_counter < back_counter do
+    temp = my_sentence[back_counter]
+    my_sentence[back_counter] = my_sentence[front_counter]
+    my_sentence[front_counter] = temp
+    front_counter += 1
+    back_counter -= 1
+  end
+
+  reverse_word(my_sentence, length)
+
+end
+
+
+def reverse_word(my_sentence, length) # do a loop table!!!
+  i = 0
+  j = 0
+  while i < length
+    j = i
+
+    while my_sentence[i] == " "
+      i += 1
+      j += 1
+    end
+
+    while my_sentence[i] != " " && my_sentence[i] != nil
+      i += 1
+    end
+    reverse(my_sentence, j, i-1)
+    i += 1
+  end
+
+end
+
+def reverse(string, beginning, ending)
+  j = beginning
+  i = ending
+
+  while j < i
+    temp = string[j]
+    string[j] = string[i]
+    string[i] = temp
+    j += 1
+    i -= 1
+  end
 end
