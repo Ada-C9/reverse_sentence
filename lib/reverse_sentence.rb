ad92f5ecee03527c2be4ba29e150db9dd29bc2a8
emilcecarlisa <emilcecarlisa@gmail.com>
2018-03-27 12:10:59 -0700

    Ranges not working properly. Needs significant refactoring

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,34 +1,43 @@
 # A method to reverse the words in a sentence, in place.
+
 def reverse_sentence(my_sentence)
   index = 0
 
   while index < my_sentence.length
-    until my_sentence[index] != " " || index >= my_sentence.length # while or until both work
+    until my_sentence[index] != ' '|| index >= my_sentence.length # while or until both work
       index += 1
     end
-
-    start_word = index
 
     until my_sentence[index] == ' ' || index >= my_sentence.length - 1
       index += 1
     end
 
-    if index == my_sentence.length - 1
+    if index == ' '
+      start_word = index + 1
+    else
+      start_word = index[0] # changed from 'index'
+    end
+
+    if index ==  ' ' || index >= my_sentence.length - 1
       end_word = index
     else
       end_word = index - 1
     end
 
-    while start_word < end_word
-      temp = my_sentence[start_word]
-      my_sentence[start_word] = my_sentence[end_word]
-      my_sentence[end_word] = temp
+    start_left = (index[start_word]..index[end_word])
+    end_right = (index[start_word]..index[end_word])
 
-      start_word += 1
-      end_word -= 1
+    while start_left < end_right
+      temp = my_sentence[start_left]
+      my_sentence[start_left] = my_sentence[end_right]
+      my_sentence[end_right] = temp
+
+      start_left += 1
+      end_right -= 1
     end
 
     index += 1
   end
+
   return my_sentence
 end
