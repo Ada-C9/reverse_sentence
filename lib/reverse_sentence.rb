a281f8f5ff4644c7b0f5368f50b5f3149273ebb8
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-03-29 11:53:12 -0700

    done

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -2,36 +2,54 @@
 require "pry"
 
 def reverse_sentence(my_sentence)
-  first = 0
+  return my_sentence if my_sentence == nil
+  start = 0
   last = my_sentence.length - 1
 
-  while my_sentence[first] == " "
-    first += 1
-  end
-  while my_sentence[last] == " "
+  while start < last
+    value = my_sentence[start]
+    my_sentence[start] = my_sentence[last]
+    my_sentence[last] = value
+    start += 1
     last -= 1
   end
-  fcount = 0
-  until my_sentence[first] == " " || my_sentence == nil
-    fcount += 1
-    first += 1
+
+  bottom = 0
+  top = 0
+
+  until bottom > (my_sentence.length - 1)
+    while my_sentence[bottom] == " "
+      bottom +=1
+      return my_sentence if bottom == (my_sentence.length)
+    end
+    word_start = bottom
+    wordlength = 0
+    word = ""
+    until my_sentence[bottom] == " " || my_sentence[bottom] == nil
+      bottom += 1
+      wordlength += 1
+      word << " "
+    end
+
+    top = wordlength - 1
+    count = 0
+    bottom = 0
+    until count > (wordlength / 2)
+      value = my_sentence[word_start + bottom]
+      word[bottom] = my_sentence[word_start + top]
+      word[top] = value
+      count += 1
+      top -= 1
+      bottom += 1
+
+    end
+    my_sentence[(word_start)..((word_start) + (wordlength - 1))] = word
+    bottom = word_start + wordlength
   end
-  first_word = my_sentence[(first - fcount)..(first - 1)]
+  puts my_sentence
 
-count = 0
-  until my_sentence[last] == " " || my_sentence == nil
-    count += 1
-    last -= 1
-  end
-  last_word = my_sentence[(last + 1 )..(last + count)]
-
-  my_sentence[(last + 1 )..(last + count)] = first_word
-
-  my_sentence[(first - fcount)..(first - 1)] = last_word
-
-binding.pry
 end
 
-sentence = " My name is wini"
 
-print reverse_sentence(sentence)
+my_sentence = " a    b    "
+puts reverse_sentence(my_sentence)
