609a59976932e19283bba4b9d9c10c63fd746b09
Steffany Brown <steffannbrown@gmail.com>
2018-04-12 09:31:59 -0700

    updated

---
+++ lib/practice.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+def reverse_sentence(my_sentence)
+
+  if my_sentence == nil
+    return nil
+  end
+
+  length = my_sentence.length
+
+  if length < 2
+    return my_sentence
+  end
+
+  front_index1 = 0
+  front_index2 = 0
+
+  back_index1 = length
+  back_index2 = length
+
+  while front_index <= back_index && back_index != nil
+    
+
+
+
+  end
+
+end
