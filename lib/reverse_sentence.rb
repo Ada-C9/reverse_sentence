b81d4228c01e4cd89c857123d8748676d1d97087
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-04-12 03:25:20 -0700

    update method

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,4 +1,16 @@
 # A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  if my_words == nil || my_words.length == 0
+    return nil
+  else
+    char_num = my_sentence.length
+    reverse_sentence = my_sentence
+    counter = 0
+    rev_counter = (letter_num-1)
+    while counter < rev_counter
+      counter += 1
+      rev_counter -= 1
+    end
+  end
+   return reverse_sentence
 end
